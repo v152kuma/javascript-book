c091e0517377b7bfb2bf9fb3b4eb1e6fc2930fbe
Vivek Kumar Yadav <vivek.yadav@vista.com>
2025-01-22 21:39:44 +0530

    adding functions module

---
+++ functions/simplefunctions.js
@@ -0,0 +1,15 @@
+
+
+const varContainingFunction = function(){
+    console.log("This is a simple function");
+};
+
+varContainingFunction();
+
+
+const addTwoNumbers = function(a, b){
+
+    return a+b;
+};
+
+console.log(addTwoNumbers(4,5));
