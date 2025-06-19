d654e647781a1de9ab963837db4a1a1187b5164d
unknown <vivek.kumar450028@gmail.com>
2025-06-19 21:05:15 +0530

    Update configuration

--- mern-poject/my-app/src/App.js
+++ mern-poject/my-app/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Data from './components/Data';
+import { Routes, Route } from 'react-router-dom';
+import DataItem from './components/DataItem';
 
 function App() {
   return (
     <div>
-     <Data />
+      <Routes>
+        <Route path="/" element={<Data />} />
+        <Route path="/:id" element={<DataItem />} />
+      </Routes>
     </div>
   );
 }
--- mern-poject/my-app/src/components/Data.jsx
+++ mern-poject/my-app/src/components/Data.jsx
@@ -1,7 +1,7 @@
 import { Card, CardHeader } from '@mui/material';
 import React from 'react';
 import { useEffect } from "react";
-import  { useState } from "react";
+import { useState } from "react";
 import { DataGrid } from '@mui/x-data-grid';
 
 
@@ -18,31 +18,37 @@ const Data = () => {
                 .then(response => response.json())
                 .then(data => setTodos(data.todos))
                 .catch(error => console.error('Error fetching todos:', error)
-                 .finally(() => setLoading(false)));
+                    .finally(() => setLoading(false)));
         }
-        
+
         fetchTodos();
 
-    }, []); 
+    }, []);
     /* Define the columns for the DataGrid */
 
     const columnns = [
-        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'id', headerName: 'ID', width: 90 ,
+            renderCell: (params) => (
+                <a href={`/${params.value}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                    {params.value}
+                </a>
+            )
+        },
         { field: 'todo', headerName: 'Todo', width: 150 },
         { field: 'completed', headerName: 'Completed', width: 120 },
         { field: 'userId', headerName: 'User ID', width: 120 }
     ];
     return (
-        <Card sx={{padding: 5}}>
+        <Card sx={{ padding: 5 }}>
             {/* Render your todos here */}
             <CardHeader title="Todos List" />
-            <DataGrid 
+            <DataGrid
                 loading={false}
-                rows={todos} 
+                rows={todos}
                 columns={columnns}
                 pageSize={5}
                 rowsPerPageOptions={[5]}
-                
+
             />
         </Card>
     )
